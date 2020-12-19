5f310a26db0c432e8a16d3d72b0f5f7b8bab0551
Reginaldo Puccinelli <puccinellirpr@gmail.com>
2020-12-19 11:48:58 -0300

    Implementação do Swagger

--- user_search/src/app.ts
+++ user_search/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from './openapi.json'
 import { router } from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(router)
 
 export { app }
